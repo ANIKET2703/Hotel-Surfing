826323c235f821abad03e75e1b95b3d1463f3544
Aniket2703 <aniketnimbalkar270320@gmail.com>
2020-08-26 19:34:22 +0530

    My page

---
+++ site/js/script.js
@@ -0,0 +1,8 @@
+var x = document.getElementByID("login");
+		var y = document.getElementByID("register");
+		var z = document.getElementByID("bt");
+		function register(){
+			x.style.left="-400px";
+			y.style.left="50px";
+			z.style.left="110px";
+		}
